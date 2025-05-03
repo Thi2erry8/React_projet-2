689ff256306063ad7239095d6c8702c949fa0ee4
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-05-02 23:04:16 -0400

    00

--- React_2/src/assets/components/MovieCard.jsx
+++ React_2/src/assets/components/MovieCard.jsx
@@ -1,11 +1,24 @@
 
 
 export default function MovieCard({movie}) {
+  function onFavoriteClick(){
+           alert("clicked")
+  }
+
   return (
     <>
        <div className="movie-card">
              <div className="movie_poster">
                 <img src={movie.url}  />
+                  <div className="movie_overlay">
+                        <button className="favorite_btn" onClick={onFavoriteClick}>
+                           ▲
+                        </button>
+                   </div>
+             </div>
+             <div className="movie_info">
+                   <h3>{movie.title}</h3>
+                   <p>{movie.realse_date}</p>
              </div>
        </div>
     </>
