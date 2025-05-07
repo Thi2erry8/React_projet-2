f87e77770a121bb2e3cc7646b88ee3ee25e8864f
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-05-06 23:00:56 -0400

    100

--- React_2/src/App.jsx
+++ React_2/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import {Routes,Route} from "react-router-dom"
 import Home from './assets/pages/Home'
-
+import Favorite from './assets/pages/Favorites'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <> 
-       <Home/>
-    </>
+    <main className="main_content">
+           <Route path="/" element={<Home/>}/>
+           <Route path="/favorites" element={<Favorite/>}/>
+    </main>
   )
 }
 
